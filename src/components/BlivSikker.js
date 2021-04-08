4882a68c55d53199d937d73dbb30b25604ed30f5
famasmaster78 <jxras11@gmail.com>
2021-04-08 10:09:51 +0200

    fix forkert routing - forside

--- src/components/BlivSikker.js
+++ src/components/BlivSikker.js
@@ -21,7 +21,107 @@ export default function BlivSikker() {
 	return (
 		<div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
 
-			Bliv sikker!
+			<Card className={classes.root} style={{ transform: "scale(1.1)" }}>
+				<CardActionArea>
+					<CardMedia
+					className={classes.media}
+					image="https://image.freepik.com/vektoren-kostenlos/premium-qualitaet-schriftzug-banner_28633-267.jpg"
+					title="Premium"
+					/>
+					<CardContent>
+					<Typography gutterBottom variant="h5" component="h2">
+						Premium
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">
+						<List component="nav" dense={true} aria-label="contacts">
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="12 beskyttede enheder" />
+							</ListItem>
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="Beskyttelse mod extreme aliens" />
+							</ListItem>
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="Beskyttelse mod 6 space threats" />
+							</ListItem>
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="Staten holder sikker backup af dine data" />
+							</ListItem>
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="Cute alien sticker" />
+							</ListItem>
+						</List>
+					</Typography>
+					</CardContent>
+				</CardActionArea>
+				<CardActions>
+					<Button size="small" color="primary" variant="contained" style={{ flexGrow: 1, fontWeight: "bold" }}>
+						Bliv ekstra sikker!
+					</Button>
+				</CardActions>
+			</Card>
+
+			<Card className={classes.root}>
+				<CardActionArea>
+					<CardMedia
+					className={classes.media}
+					image="https://marltonag.org/wp-content/uploads/2018/06/1.png"
+					title="Premium"
+					/>
+					<CardContent>
+					<Typography gutterBottom variant="h5" component="h2">
+						Basic
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">
+						<List component="nav" dense={true} aria-label="contacts">
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="3 beskyttede enheder" />
+							</ListItem>
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="Beskyttelse mod aliens" />
+							</ListItem>
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="Beskyttelse mod 2 space threats" />
+							</ListItem>
+							<ListItem>
+								<ListItemIcon>
+									<FiberManualRecordIcon style={{ fontSize: "small" }} />
+								</ListItemIcon>
+								<ListItemText primary="Staten holder backup af dine data" />
+							</ListItem>
+						</List>
+					</Typography>
+					</CardContent>
+				</CardActionArea>
+				<CardActions>
+					<Button size="small" color="primary" variant="contained" style={{ flexGrow: 1, fontWeight: "bold" }}>
+						Bliv sikker!
+					</Button>
+				</CardActions>
+			</Card>
 
 		</div>
 	);
